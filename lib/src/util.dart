b2bfbfe9391a2fd3ce1d6481f465b0eda174745f
Volodymyr Smolianinov <voidozzer@gmail.com>
2020-12-06 23:21:51 +0100

    Utils update

--- lib/src/util.dart
+++ lib/src/util.dart
@@ -1,7 +1,85 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart' as auth;
+import 'package:flutter/material.dart';
 import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
 import 'package:flutter_firebase_chat_core/src/models/room.dart';
+
+import '../flutter_firebase_chat_core.dart';
+
+class CreateChatData {
+  const CreateChatData({
+    @required this.otherUser,
+  });
+
+  final types.User otherUser;
+}
+
+class CreateGroupData {
+  const CreateGroupData({
+    this.imageUrl,
+    @required this.name,
+    @required this.users,
+  });
+
+  final String imageUrl;
+  final String name;
+  final List<types.User> users;
+}
+
+Future<Room> createRoom(auth.User firebaseUser, dynamic roomData) async {
+  if (roomData is CreateChatData) {
+    final query = await FirebaseFirestore.instance
+        .collection('rooms')
+        .where('userIds', arrayContains: firebaseUser.uid)
+        .get();
+
+    final rooms = await processRoomsQuery(firebaseUser, query);
+
+    final existingRoom = rooms.firstWhere((room) {
+      if (room.isGroup) return false;
+
+      final userIds = room.users.map((u) => u.id);
+      return (userIds.contains(firebaseUser.uid) &&
+          userIds.contains(roomData.otherUser.id));
+    });
+
+    if (existingRoom != null) {
+      return existingRoom;
+    }
+  }
+
+  final currentUser = await fetchUser(firebaseUser.uid);
+
+  final imageUrl = (roomData is CreateChatData) ? null : roomData.imageUrl;
+  final isGroup = (roomData is CreateGroupData);
+  final name = (roomData is CreateChatData) ? null : roomData.name;
+  final users = (roomData is CreateChatData)
+      ? [currentUser, roomData.otherUser]
+      : [currentUser] + roomData.users.map((u) => u);
+
+  final room = await FirebaseFirestore.instance.collection('rooms').add({
+    'imageUrl': imageUrl,
+    'isGroup': isGroup,
+    'userIds': users.map((u) => u.id),
+    'name': name,
+  });
+
+  return new Room(
+    id: room.id,
+    isGroup: isGroup,
+    users: users,
+    name: name,
+    imageUrl: imageUrl,
+  );
+}
+
+void createUserInFirestore(types.User user) async {
+  await FirebaseFirestore.instance.collection('users').doc(user.id).set({
+    'avatarUrl': user.avatarUrl,
+    'firstName': user.firstName,
+    'lastName': user.lastName,
+  });
+}
 
 Future<types.User> fetchUser(String userId) async {
   final doc =
